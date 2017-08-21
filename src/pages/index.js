db54cced77abc090669f999a205c703315a2ce31
Andrew Oh <andrewoh531@gmail.com>
2017-08-21 12:29:56 +1000

    Adding Google Analytics tracking id

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Intro from './intro/Intro';
 import Testimonials from './testimonials/TestimonialCarousel';
 import OurDifference from './our-difference/OurDifference';
-//import AboutUs from '../about-us/AboutUs';
 import OurServices from './our-services/OurServices';
 import Faq from './faq/Faq';
 import ContactUs from './contact-us/ContactUs';
 import './App.css';
+//import AboutUs from '../about-us/AboutUs';
 
 class App extends Component {
   render() {
