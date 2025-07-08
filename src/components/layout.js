13d65b2835b41feb14129b3ab22e4df9ea0bb147
andrewoh531 <andrewoh531@gmail.com>
2025-07-08 22:56:29 +1000

    Add additional meta tags and JSON-LD

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,6 +19,30 @@ export default ({ children }) => (
           titleTemplate={`%s | ${data.site.siteMetadata.title}`}
           defaultTitle={data.site.siteMetadata.title}
         >
+          <meta name="description" content="Pristine Clean offers professional, reliable, and eco-friendly home and office cleaning services. Relax and leave the cleaning to us! Serving Sydney and surrounding areas." />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="canonical" href="https://pristineclean.com.au/" />
+          {/* Structured data for LocalBusiness will be added here */}
+          <script type="application/ld+json">
+            {`
+              {
+                "@context": "https://schema.org",
+                "@type": "LocalBusiness",
+                "name": "Pristine Clean",
+                "telephone": "+61 0426 262 338",
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Burdett St",
+                  "addressLocality": "Hornsby",
+                  "addressRegion": "NSW",
+                  "postalCode": "2077",
+                  "addressCountry": "AU"
+                },
+                "url": "https://pristineclean.com.au",
+                "priceRange": "$$"
+              }
+            `}
+          </script>
           <link
             href="https://fonts.googleapis.com/css?family=Raleway"
             rel="stylesheet"
